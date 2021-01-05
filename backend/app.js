e9f992ba13f4bd6bec3f473a12643ddd441e194e
dle <david.le@aubay.com>
2021-01-05 10:41:36 +0100

    correctif docker

--- backend/app.js
+++ backend/app.js
@@ -11,14 +11,14 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-const PORT = process.env.PORT || 5000;
+const PORT = 4242;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
 // set up mongoose
 
 mongoose.connect(
-  process.env.MONGODB_CONNECTION_STRING,
+  process.env.MONGODB_DOCKER,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
